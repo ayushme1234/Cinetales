dc5d6eee7efb9935ce1e91077097ffe371dc7f49
Ayush <112809832+ayushme1234@users.noreply.github.com>
2026-05-02 02:38:18 +0530

    Update sitemap response headers
    Change Content-Type header to application/xml and add Cache-Control header.

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -17,18 +17,15 @@ export async function getServerSideProps({ res }) {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages
-  .map(
-    (page) => `  <url>
+${pages.map((page) => `  <url>
     <loc>${baseUrl}${page}</loc>
     <changefreq>weekly</changefreq>
     <priority>${page === "" ? "1.0" : "0.8"}</priority>
-  </url>`
-  )
-  .join("\n")}
+  </url>`).join("\n")}
 </urlset>`;
 
-  res.setHeader("Content-Type", "text/xml");
+  res.setHeader("Content-Type", "application/xml");
+  res.setHeader("Cache-Control", "public, s-maxage=86400");
   res.write(sitemap);
   res.end();
 
